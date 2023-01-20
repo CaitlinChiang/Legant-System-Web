b5606143ad84b4df7ad841778eac8ac44ffad927
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-20 21:34:19 +0800

    Fix

---
+++ src/backend/controllers/reviews/queries/index.ts
@@ -0,0 +1,9 @@
+import get_review from './get_review'
+import get_reviews from './get_reviews'
+import get_reviews_count from './get_reviews_count'
+
+export default {
+  get_review,
+  get_reviews,
+  get_reviews_count
+}

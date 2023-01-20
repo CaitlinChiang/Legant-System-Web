8f0882239d31ac9c7f25e33dd9f472eaa602b32d
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-20 18:03:56 +0800

    Create enums

---
+++ src/enums/paymentStatus.ts
@@ -0,0 +1,6 @@
+export enum PaymentStatus {
+  COMPLETE = 'COMPLETE',
+  PENDING = 'PENDING',
+  REFUNDED = 'REFUNDED',
+  CANCELLED = 'CANCELLED'
+}
---
+++ src/enums/stockQuantity.ts
@@ -0,0 +1,11 @@
+export enum StockQuantityAction {
+  ADD = 'ADD',
+  SUBTRACT = 'SUBTRACT'
+}
+
+export enum StockQuantityOperator {
+  ABOVE = 'ABOVE',
+  BELOW = 'BELOW',
+  BETWEEN = 'BETWEEN',
+  EQUAL = 'EQUAL'
+}

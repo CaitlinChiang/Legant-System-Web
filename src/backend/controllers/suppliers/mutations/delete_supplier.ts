e6f14b532484df1d00e26afc9ce63ff69a53bfa5
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-22 18:23:16 +0800

    delete products when supplier deleted

--- src/backend/controllers/suppliers/mutations/delete_supplier.ts
+++ src/backend/controllers/suppliers/mutations/delete_supplier.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import { Context } from '../../../../types/setup/context'
 import { DeleteSupplier } from '../../../../types/supplier'
 import { deleteData } from '../../../_utils/handleData/deleteData'
@@ -8,4 +9,6 @@ export default async (
   context: Context
 ): Promise<void> => {
   await deleteData(context, args, 'suppliers')
+
+  await context.database.products.deleteMany({ supplierId: new ObjectId(args._id) })
 }

77837a26d18e5a3668e89ee5771a00062dd857e8
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-25 09:04:14 +0800

    Reconfigure stockQuantity logic

--- src/backend/_utils/handleData/updateStockQuantity.ts
+++ src/backend/_utils/handleData/updateStockQuantity.ts
@@ -1,21 +1,54 @@
 import { Context } from '../../../types/setup/context'
-import { PackageItem } from '../../../types/package'
+import { CartItem } from '../../../types/cart'
+import { Package, PackageItem } from '../../../types/package'
 import { StockQuantityAction } from '../../../enums/stockQuantity'
+import { returnData } from '../handleData/returnData'
 
 export const updateStockQuantity = async (
+  context: Context,
+  action: StockQuantityAction,
+  items: CartItem[]
+): Promise<void> => {
+  await updatePackageStockQty(context, action, items)
+}
+
+const updatePackageStockQty = async (
+  context: Context,
+  action: StockQuantityAction,
+  items: CartItem[]
+): Promise<void> => {
+  for (let i = 0, n = items.length; i < n; i++) {
+    const { packageId, quantity }: CartItem = items[i]
+
+    const stockQty: number =
+      action === StockQuantityAction.ADD ? quantity : -quantity
+
+    await context.database.packages.findOneAndUpdate(
+      { _id: packageId },
+      { $inc: { stockQty } }
+    )
+
+    // UPDATING STOCK QUANTITY OF THE PACKAGE'S PRODUCTS
+    const pack: Package = await returnData(context, { _id: packageId }, 'packages')
+
+    await updateProductStockQty(context, action, pack.items)
+  }
+}
+
+const updateProductStockQty = async (
   context: Context,
   action: StockQuantityAction,
   items: PackageItem[]
 ): Promise<void> => {
   for (let i = 0, n = items.length; i < n; i++) {
-    const { productId, quantity } = items[i]
+    const { productId, quantity }: PackageItem = items[i]
 
-    const stockQuantity: number =
+    const stockQty: number =
       action === StockQuantityAction.ADD ? quantity : -quantity
 
     await context.database.products.findOneAndUpdate(
       { _id: productId },
-      { $inc: { stockQuantity } }
+      { $inc: { stockQty } }
     )
   }
 }
